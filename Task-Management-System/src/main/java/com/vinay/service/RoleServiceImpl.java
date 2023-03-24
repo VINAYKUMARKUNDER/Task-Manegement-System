d8018e1722973b5eae06260ee980b8052990d0ff
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-03-24 08:47:54 +0530

    Service layer created

---
+++ Task-Management-System/src/main/java/com/vinay/service/RoleServiceImpl.java
@@ -0,0 +1,5 @@
+package com.vinay.service;
+
+public class RoleServiceImpl implements RoleService {
+
+}
---
+++ Task-Management-System/src/main/java/com/vinay/service/UserServiceImpl.java
@@ -0,0 +1,5 @@
+package com.vinay.service;
+
+public class UserServiceImpl implements UserService {
+
+}
