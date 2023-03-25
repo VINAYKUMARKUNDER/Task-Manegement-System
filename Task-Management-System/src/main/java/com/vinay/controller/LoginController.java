fa46f13f10099765342f0463fe157e901856e9fd
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-03-25 19:22:50 +0530

    changes in pom.xml file

--- Task-Management-System/src/main/java/com/vinay/controller/LoginController.java
+++ Task-Management-System/src/main/java/com/vinay/controller/LoginController.java
@@ -8,7 +8,6 @@ public class LoginController {
 
     @GetMapping("/login")
     String showLoginForm() {
-        //login form is submitted using POST method <form th:action="@{/login}" method="post">
         return "forms/login";
     }
 }
